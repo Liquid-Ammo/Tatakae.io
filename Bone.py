76433917c86637f7d692e8e0641dc12c3e111eab
Liquid_Ammo <167435248+Liquid-Ammo@users.noreply.github.com>
2024-06-17 19:39:20 +0000

    added 2d matix and added attack to the matix with jump and duck

--- Bone.py
+++ Bone.py
@@ -1,14 +1,13 @@
 # modules
-import pygame
 from subprocess import call  # for clear
 import os  # for clear
 from time import sleep  # for time delay
 from Attack import attack, defence ,  filler , Moves
-
+print(attack)
 # to clear the screen
 def clear():
-  _ = call("clear" if os.name == "posix" else "cls")
-
+    _ = call("clear" if os.name == "posix" else "cls")
+    
 
 clear()
 
@@ -20,112 +19,108 @@ p1_ck = 1500
 p2_ck = 1500
 
 # 6 by 10 matrix for the game back bone
-a0 = [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "]
-a1 = [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "]
-a2 = [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "]
-a3 = ["O", 0, 0, 0, 0, 0, 0, 0, 0, "O"]
-a4 = ["|", 0, 0, 0, 0, 0, 0, 0, 0, "|"]
-a5 = ["|", 0, 0, 0, 0, 0, 0, 0, 0, "|"]
-a6 = ["^", 0, 0, 0, 0, 0, 0, 0, 0, "^"] 
-print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
 
-# functions
+a=[ [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "],
+    [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "], 
+    [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "],
+    [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "], 
+    [" ", 0, 0, 0, 0, 0, 0, 0, 0, " "],
+    ["O", 0, 0, 0, 0, 0, 0, 0, 0, "O"], 
+    ["|", 0, 0, 0, 0, 0, 0, 0, 0, "|"], 
+    ["|", 0, 0, 0, 0, 0, 0, 0, 0, "|"],  
+    ["^", 0, 0, 0, 0, 0, 0, 0, 0, "^"]]
 
 
-# jumping
+
+def printer(c=1):
+    clear()
+    for i in a:
+        if c==1:
+            c+=1
+            continue
+        print(i)
+    print()
+printer()
+
+
+def p2i(c):
+    if c==1:
+        n=0
+    elif c==2:
+        n=9
+    return n
+
 def jump(c=1):
-    n = 10
-    if c == 1:
-        n = 0
-    if c == 2:
-        n = 9
-    sleep(0.3)
-    clear()
-    for i in range(2):
-        print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
-        a1[n] = a2[n]
-        a2[n] = a3[n]
-        a3[n] = a4[n]
-        a4[n] = a5[n]
-        a5[n] = a6[n]
-        a6[n] = a0[n]
-        sleep(0.1)
-        print()
-        clear()
-    for i in range(2):
-        print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
-        a6[n] = a5[n]
-        a5[n] = a4[n]
-        a4[n] = a3[n]
-        a3[n] = a2[n]
-        a2[n] = a1[n]
-        a1[n] = a0[n]
-        sleep(0.1)
-        print()
-        clear()
-    print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
+    n=p2i(c)
+    
+    if a[5][n]=="O":
+        #up
+        for j in range (2): 
+            sleep(0.1)
+            for i in range (1,9,1):
+                a[i-1][n]=a[i][n]
+                if i ==8:
+                    a[8][n]=a[0][n]
+            printer()    
+            
+        #down
+        for j in range (2): 
+            sleep(0.1)
+            for i in range (8,-1,-1):
+                if i ==8:
+                    a[0][n]=a[8][n]
+                else:
+                    a[i+1][n]= a[i][n]
+                printer()
+            printer()
+    else:
+        pass
 
-
-# ducking
-def duck(c):
-    n = 4
-    if c == 1:
-        n = 0
-    if c == 2:
-        n = 9
-    sleep(0.3)
-    clear()
-    if True:
-        print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
-        a0[n] = a4[n]
-        a4[n] = a3[n]
-        a3[n] = a2[n]
-        sleep(0.25)
-        print()
-        clear()
-    if True:
-        print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
-        a3[n] = a4[n]
-        a4[n] = a0[n]
-        a0[n] = a1[n]
-        sleep(0.25)
-        print()
-        clear()
-    print(a0, a1, a2, a3, a4, a5, a6, sep="\n")
-
-def move_classification(x):
+def duck (c=1):
+    n=p2i(c)
+    
+    #down
+    if a[5][n]=="O":
+        a[0][n]=a[6][n]
+        a[6][n]=a[5][n]
+        a[5][n]=a[2][n]
+        printer()
+        sleep(0.3)
+    if a[6][n]=="O":
+        a[5][n]=a[6][n]
+        a[6][n]=a[0][n]
+        a[0][n]=a[1][n]
+        printer()
+    
+def move(x,y):
     if x in attack:
-        a = 1
+        attacks(x,y)
     elif x in defence:
-        a = 2
+        defence(x,y)
     elif x in filler:
-        a = 3
+        filler(x,y)
     else:
         print("Attack type error")
     return a
 
-def move(x,y):
-    a=move_classification(x)
-    if a==1:
-        attack(x,y)
 
-def attack(x,c):
-    n = 10
-    if c == 1:
-        n = 0
-    if c == 2:
-        n = 9
-    
-    if a1[n] == "|":
-        for i in range (2,9):
-            pass
-  
+def attacks(x,y):
+    n = p2i(y)
+    for i in range (len(a)):
+        if a[i][n]=="|":
+            for j in range(1,9,1):
+                a[i][j]=3
+                sleep(0.05)
+                printer()
+                a[i][j]=0  
+            break
 
-# inputs for now will be part of pygame later
-for i in range(0, 4):
-    a = int(input("Jump : "))
-    jump(a)
-    b = int(input("Duck : "))
-    duck(b)
-    c = tuple(input("Move : "))
-    move(c)
+
+
+for i in range (2):
+    ae=int(input("jump"))
+    jump(ae)
+    be=int(input("duck"))
+    duck(be)
+    ce=int(input("attack"))
+    move("Eye Gouge",ce)
